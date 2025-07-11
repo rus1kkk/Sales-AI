e1fabcdb970fc85a65b6e1dd2111faf8242e89eb
RingletN <nastasya-2004@mail.ru>
2025-07-11 22:31:35 +0300

    feat: add all figures and animations

--- frontend/src/components/Background/FloatingShapes.vue
+++ frontend/src/components/Background/FloatingShapes.vue
@@ -11,6 +11,7 @@
       class="custom-shape top-right"
       :src="generateFigure_1"
       alt="Generate Figure"
+       id="flower-figure"
     />
     <img
       v-if="$route.name === 'GeneratePage'"
@@ -18,13 +19,28 @@
       :src="generateFigure_2"
       alt="Generate Figure 2"
     />
+    <img
+    v-if="$route.name === 'profile'"
+    class="custom-shape top-right"
+    :src="profileFigure_1"
+    alt="Profile Figure"
+  />
+  <img
+    v-if="$route.name === 'profile'"
+    class="custom-shape bottom-left"
+    id="chain-figure"
+    :src="profileFigure_2"
+    alt="Profile Figure 2"
+  />
   </div>
 </template>
 
 <script>
-import mainFigure from '@/assets/images/figure2.png'
-import generateFigure_1 from '@/assets/images/figure3.png'
-import generateFigure_2 from '@/assets/images/figure4.png'
+import mainFigure from '@/assets/images/figure1.png'
+import generateFigure_1 from '@/assets/images/figure2.png'
+import generateFigure_2 from '@/assets/images/figure3.png'
+import profileFigure_1 from '@/assets/images/figure4.png'
+import profileFigure_2 from '@/assets/images/figure5.png'
 
 export default {
   data() {
@@ -32,8 +48,10 @@ export default {
       mainFigure,
       generateFigure_1,
       generateFigure_2,
+      profileFigure_1,
+      profileFigure_2,
     }
-  },
+  }
 }
 </script>
 
