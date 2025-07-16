cc1623d5c63af16a081a012c843ae5cc43d33fb5
killer_rusik <ruslan.pivnik@yandex.ru>
2025-07-17 02:09:37 +0300

    formatting commit

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,3 +1,5 @@
+<?php
+
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
@@ -9,10 +11,10 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
-   Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-   Route::get('/me', [AuthController::class, 'me'])->name('me');
-   Route::post('/password/change', [AuthController::class, 'changePassword'])->name('password.change');
-   Route::patch('/profile/update', [AuthController::class, 'updateProfile'])->name('profile.update');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/me', [AuthController::class, 'me'])->name('me');
+    Route::post('/password/change', [AuthController::class, 'changePassword'])->name('password.change');
+    Route::patch('/profile/update', [AuthController::class, 'updateProfile'])->name('profile.update');
 });
 
 Route::post('/requirements/generate', [RequirementController::class, 'generate']);
